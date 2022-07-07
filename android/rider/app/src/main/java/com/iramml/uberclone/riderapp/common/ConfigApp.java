864f99bf9c71892bfb485322f014e4b6d45a7de0
saurav <Vsaurav026@gmail.com>
2022-07-07 10:13:24 +0530

    v2.0.0

--- android/rider/app/src/main/java/com/iramml/uberclone/riderapp/common/ConfigApp.java
+++ android/rider/app/src/main/java/com/iramml/uberclone/riderapp/common/ConfigApp.java
@@ -4,7 +4,7 @@ public class ConfigApp {
     public static final double baseFare = 2.55;
     public static final double timeRate = 0.35;
     public static final double distanceRate = 1.75;
-    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAF3zIV-w:APA91bHjuWHfpd5-8_ZTkYC4_atq8nmcfvYPxgt12cle0IbOW6YAFhaZ3nvLZQj2GK30FBGVMrpsl3-kcu-0OOOtbvJgtCIIMNoOvsJaoy-ulDlnDSsMQD4w5b9EeTMp1mTs6kHTyZcY";
-    public static final String GOOGLE_API_KEY = "AIzaSyDfrhyZAZpsoVmz4NaVVJLZC91MINPGr9s";
+    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAY0flt-0:APA91bEkW4s3UUzCKX7OVeB-JosV6Ed1todn9lABKK3FsSeZKlnWQwsMp_itI0bM9D0lt3bzt1MMER8CqGPyRFFLrjOyTi2749cqRoGmxyLCY8o0Ru_N4COfKfW_x-GKFd4d1XpIrs3J";
+    public static final String GOOGLE_API_KEY = "AIzaSyAs5djlbQP4sGZcLum5xkjYGg2zT2u02pY";
 
 }
